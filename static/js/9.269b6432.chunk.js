3b6a91d12b71095d0bcaaa824ad10aea3df7c3c6
Nikita Sokolov <sokolovnd@sushivesla.team>
2021-07-17 17:09:00 +0300

    Updates

--- static/js/9.269b6432.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpinvest-app"]=this["webpackJsonpinvest-app"]||[]).push([[9],{175:function(t,a,e){"use strict";e.r(a);for(var s=e(284),i=e(291),n=e(289),r=e(10),o=[],l=0;l<12;l++){var p;o.push(null===(p=i.a.localize)||void 0===p?void 0:p.month(l))}a.default=function(){return Object(r.jsx)(n.b,{marginStart:"size-100",marginEnd:"size-100",children:Object(r.jsx)(s.a,{data:{labels:o,datasets:[{label:"My First Dataset",data:[65,59,80,81,56,55,40],fill:!1,borderColor:"rgb(75, 192, 192)",tension:.1}]},options:{},type:"line"})})}}}]);
-//# sourceMappingURL=9.269b6432.chunk.js.map
